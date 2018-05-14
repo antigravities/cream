f17d2073291789c7035c7febffbc63f6ffefd9f3
Alexandra Frock <im@alexandra.moe>
2018-05-14 03:38:13 -0400

    Import

--- client/submit.js
+++ client/submit.js
@@ -1,17 +1,21 @@
-(function(){
-	try {
+(function() {
+  try {
     var payload = Array.prototype.slice.call(document.querySelectorAll("a.search_result_row")).map((i) => {
       let g = {};
       g.appid = parseInt(i.getAttribute("data-ds-appid"));
       g.title = i.querySelector(".title").innerText.trim();
 
       let price;
-      if( i.querySelector(".search_price.discounted") === null ) price = i.querySelector(".search_price").innerText.trim();
+
+      g.oprice = i.querySelector(".search_price").innerText.trim().split("\n")[0];
+      g.oprice = g.oprice === "" ? -1 : parseFloat(g.oprice.substring(1));
+
+      if (i.querySelector(".search_price.discounted") === null) price = i.querySelector(".search_price").innerText.trim();
       else price = i.querySelector(".search_price.discounted").innerText.split("\n")[1].trim();
-      
-      if( price.indexOf("Free") > -1 ) price = 0.00;
-      else if( price === "" ) price = -1;
-      else if( price[0] != "$" ) throw price;
+
+      if (price.indexOf("Free") > -1) price = 0.00;
+      else if (price === "") price = -1;
+      else if (price[0] != "$") throw price;
       else price = parseFloat(price.substring(1));
 
       g.discount = parseInt(i.querySelector(".search_discount").innerText.replace("-", "").replace("%", ""));
@@ -20,13 +24,13 @@
       g.windows = i.querySelector(".win") === null ? false : true;
       g.macos = i.querySelector(".mac") === null ? false : true;
       g.linux = i.querySelector(".linux") === null ? false : true;
-      
+
       g.htcvive = i.querySelector(".htcvive") === null ? false : true;
       g.oculusrift = i.querySelector(".oculusrift") === null ? false : true;
       g.windowsmr = i.querySelector(".windowsmr") === null ? false : true;
-      
+
       let sum = i.querySelector(".search_review_summary");
-      if( sum === null ) g.reviews = "Not enough reviews";
+      if (sum === null) g.reviews = "Not enough reviews";
       else g.reviews = sum.getAttribute("data-tooltip-html").split("<br>")[0];
 
       g.releasedate = i.querySelector(".search_released").innerText;
@@ -35,5 +39,6 @@
 
       return g;
     });
-  } catch(e){ console.log(e); }
+  }
+  catch (e) { console.log(e); }
 })();
---
+++ server/database.js
@@ -0,0 +1,79 @@
+/* global config */
+
+function getObjectClass(obj){
+  return Object.prototype.toString.call(obj).slice(8, -1);
+}
+
+class Database {
+  constructor(){
+    this.MySQL = require("promise-mysql");
+    this.isConnected = false;
+  }
+  
+  async connect(){
+    try {
+      this.connection = await this.MySQL.createConnection(config.mysql);
+      this.isConnected = true;
+    } catch(e){
+      throw e;
+    }
+  }
+  
+  async _query(query, values){
+    return this.killBuffers(await this.connection.query(query, values));
+  }
+  
+  async _first(query, values){
+    let response = await this._query(query, values);
+    
+    if( response.length === 0 ) return null;
+    else return response[0];
+  }
+  
+  async _count(query, values){
+    let response = await this._query(query, values);
+    
+    if( response.length === 0 ) return 0;
+    else return response[0].result;
+  }
+  
+  async _insert(query, values){
+    let response = await this.connection.query(query, values);
+    return response.insertId;
+  }
+  
+  async _is(query, values){
+    let response = await this._first(query, values);
+    return response.result > 0;
+  }
+  
+  killBuffers(obj){
+    if( getObjectClass(obj) != "Array" ) obj = [ obj ];
+    
+    obj.forEach((n) => {
+      Object.keys(n).forEach((v) => {
+        if( getObjectClass(n[v]) == "Uint8Array" ) n[v] = n[v].readInt8(0); 
+      });
+    });
+    
+    return obj;
+  }
+  
+  async getUserByApiKey(apikey){
+    return await this._first("SELECT * FROM users WHERE apikey = ? LIMIT 1", [ apikey ]);
+  }
+  
+  async getUnverifiedApp(app){
+    return await this._first("SELECT * FROM apps_unverified WHERE appid = ?", [ app ]);
+  }
+  
+  async addApp(table, appid, title, oprice, price, discount, windows, macos, linux, htcvive, oculusrift, windowsmr, reviews, releasedate, submitter){
+    return await this._query("INSERT INTO apps" + table + " (appid, title, oprice, price, discount, windows, macos, linux, htcvive, oculusrift, windowsmr, reviews, releasedate, submitter) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, FROM_UNIXTIME(?), ?)", [ appid, title, oprice, price, discount, windows, macos, linux, htcvive, oculusrift, windowsmr, reviews, releasedate, submitter ])
+  }
+  
+  async deleteApp(table, appid){
+    return await this._query("DELETE FROM apps" + table + " WHERE appid = ?", [ appid ]);
+  }
+}
+
+module.exports = Database;
